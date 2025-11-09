9f22017dc6405ecb3a8227abd8671786dad5fa4f
Muhd Amirul <amirulhaziqazli14@gmail.com>
2025-11-09 15:24:54 +0800

    <html>

--- Home.php
+++ Home.php
@@ -4,8 +4,8 @@ $loggedIn = isset($_SESSION['username']);
 $username = $loggedIn ? $_SESSION['username'] : 'Guest';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
+<html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
